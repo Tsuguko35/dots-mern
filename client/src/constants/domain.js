0c3ed3334fdb9c2bdd5e024b9830ecec71beb240
Jazpher Carpio <johnjazpher.carpio.dc@bulsu.edu.ph>
2024-03-24 13:32:32 +0800

    deployment

--- client/src/constants/domain.js
+++ client/src/constants/domain.js
@@ -1,5 +1,5 @@
-// const domain = 'https://cvrs.slarenasitsolutions.com/'
+const domain = "https://cvrs.slarenasitsolutions.com:8080/";
 
-const domain = 'http://localhost:8080/'
+// const domain = 'http://localhost:8080/'
 
-export default domain
+export default domain;
