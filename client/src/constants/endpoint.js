7e01a3b9e63c35362ba84652c5f4dec7ef6eb805
Jazpher Carpio <johnjazpher.carpio.dc@bulsu.edu.ph>
2024-03-27 07:43:26 +0800

    render

--- client/src/constants/endpoint.js
+++ client/src/constants/endpoint.js
@@ -1,5 +1,5 @@
 // const ENDPOINT = "https://cvrs.slarenasitsolutions.com:8080";
 
-const ENDPOINT = "http://localhost:8080/";
+const ENDPOINT = "https://dots-mern.onrender.com/";
 
 export default ENDPOINT;
