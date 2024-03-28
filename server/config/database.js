93100eb83f061a94e65ab5d2dbba4d67358a74f9
Jazpher Carpio <johnjazpher.carpio.dc@bulsu.edu.ph>
2024-03-28 16:14:16 +0800

    keepSQLAlive

--- server/config/database.js
+++ server/config/database.js
@@ -1,13 +1,15 @@
-import mysql from 'mysql2'
-import dotenv from 'dotenv'
+import mysql from "mysql2";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const db = mysql.createConnection({
-    host: process.env.SQL_HOST,
-    user: process.env.SQL_USERNAME,
-    password: process.env.SQL_PASSWORD,
-    database: process.env.SQL_DATABASE
-})
+  host: process.env.SQL_HOST,
+  user: process.env.SQL_USERNAME,
+  password: process.env.SQL_PASSWORD,
+  database: process.env.SQL_DATABASE,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 30000,
+});
 
-export default db
+export default db;
