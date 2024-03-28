d51741d835c2e1cf52c2a660a013df1c09df23ca
Jazpher Carpio <johnjazpher.carpio.dc@bulsu.edu.ph>
2024-03-28 22:53:43 +0800

    pool

--- server/config/database.js
+++ server/config/database.js
@@ -3,13 +3,32 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.SQL_HOST,
   user: process.env.SQL_USERNAME,
   password: process.env.SQL_PASSWORD,
   database: process.env.SQL_DATABASE,
-  enableKeepAlive: true,
-  keepAliveInitialDelay: 30000,
+  waitForConnections: true, // Wait for available connection if connection limit is reached
+  connectionLimit: 10, // Maximum number of connections to create at once
+  queueLimit: 0, // Unlimited queueing requests (default)
+});
+
+// Handle connection errors
+db.on("error", (err) => {
+  console.error("Database connection error:", err);
+  if (err.code === "PROTOCOL_CONNECTION_LOST") {
+    // Re-establish connection
+    db.getConnection((error, connection) => {
+      if (error) {
+        console.error("Error reconnecting:", error);
+      } else {
+        console.log("Reconnected to database.");
+        connection.release();
+      }
+    });
+  } else {
+    throw err;
+  }
 });
 
 export default db;
