2bb46a8a83bdd02f1e2ac1b411df40ebb634bcfa
Jazpher Carpio <johnjazpher.carpio.dc@bulsu.edu.ph>
2024-03-28 19:56:29 +0800

    createPool

--- server/config/database.js
+++ server/config/database.js
@@ -3,13 +3,18 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.SQL_HOST,
   user: process.env.SQL_USERNAME,
   password: process.env.SQL_PASSWORD,
   database: process.env.SQL_DATABASE,
+  waitForConnections: true, // Optionally, wait for connections instead of throwing errors
+  connectionLimit: 50, // Adjust as per your requirements
+  queueLimit: 0, // No limit on the number of waiting connections
   enableKeepAlive: true,
   keepAliveInitialDelay: 30000,
 });
+
+const db = pool.promise(); // Using promise wrapper for easier async/await usage
 
 export default db;
