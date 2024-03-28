3462d1a2d0045b1beca6579985cdb45a84022ad8
Jazpher Carpio <johnjazpher.carpio.dc@bulsu.edu.ph>
2024-03-28 20:05:31 +0800

    test sign in

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -21,41 +21,46 @@ const signIn = asyncHandler(async (req, res) => {
   const { username, password } = req.body;
   const q = `SELECT * FROM users WHERE email = '${username}' LIMIT 1`;
 
-  db.query(q, async (err, user) => {
-    if (err) return res.json({ success: false });
-
-    if (user && user.length > 0) {
-      const passwordMatch = await bcrypt.compare(password, user[0].password);
-
-      if (!passwordMatch) {
-        return res
-          .status(400)
-          .json({ errorMessage: "Invalid email or password." });
-      } else {
-        const token = generateToken(user[0].user_id);
-
-        res.cookie("token", token, {
-          httpOnly: true,
-          secure: true,
-          sameSite: "none",
-        });
-        res.status(200).json({
-          user_id: user[0].user_id,
-          role: user[0].role,
-          email: user[0].email,
-          profilePic: user[0].profile_Pic,
-          full_Name: user[0].full_Name,
-          date_Created: user[0].date_Created,
-          status: user[0].status,
-          token,
-        });
-      }
-    } else {
+  try {
+    const [rows, fields] = await db.query(q);
+    if (rows.length === 0) {
       return res
         .status(400)
         .json({ errorMessage: "Invalid email or password." });
     }
-  });
+
+    const user = rows[0];
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res
+        .status(400)
+        .json({ errorMessage: "Invalid email or password." });
+    }
+
+    const token = generateToken(user.user_id);
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    });
+
+    res.status(200).json({
+      user_id: user.user_id,
+      role: user.role,
+      email: user.email,
+      profilePic: user.profile_Pic,
+      full_Name: user.full_Name,
+      date_Created: user.date_Created,
+      status: user.status,
+      token,
+    });
+  } catch (error) {
+    console.error("Error signing in:", error);
+    res
+      .status(500)
+      .json({ errorMessage: "An error occurred while signing in." });
+  }
 });
 
 const deleteUser = asyncHandler(async (req, res) => {
